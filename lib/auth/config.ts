4c915498989c075c2628114190fb905dcd3ae2f3
TrevorGall16 <103269809+TrevorGall16@users.noreply.github.com>
2026-02-13 21:58:15 +0100

    dz

--- lib/auth/config.ts
+++ lib/auth/config.ts
@@ -22,7 +22,7 @@ export const authOptions: NextAuthOptions = {
   ],
 
   session: {
-    strategy: "database",
+    strategy: "jwt",
     maxAge: 30 * 24 * 60 * 60, // 30 days
     updateAge: 24 * 60 * 60, // 24 hours
   },
